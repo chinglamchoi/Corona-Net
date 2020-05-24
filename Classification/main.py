742609e85832ddc2f9e5f63cbf31eab292ce0bc8
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-24 18:40:57 +0800

    Add files via upload

--- Classification/main.py
+++ Classification/main.py
@@ -1,5 +1,7 @@
 import torch
 import torch.utils.data as data
+import torchvision.transforms as transforms
+import torch.optim as optim
 import numpy as np
 import os
 from efficientnet import EfficientNet
@@ -12,23 +14,25 @@ class CoronaNet(nn.Module):
         super().__init__()
         self.model = EfficientNet.from_pretrained("efficientnet-b5").to(device)
         self.dp = nn.Dropout(p=0.4)
-        self.ln = nn.Linear(in_features=1000, out_features=3, bias=True)
+        self.ln = nn.Linear(in_features=1000, out_features=1, bias=True)
     def forward(self, x):
         out = nn.functional.relu(self.model(x))
-        out = nn.ln(self.dp(out))
+        out = out.view(out.size(0), -1)
+        out = self.ln(self.dp(out))
         return out
 
 def run_cnn():
     return CoronaNet()
 
 class Covid(data.Dataset):
-    def __init__(self, imgs, lbs, transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize([-377.6907247987177], [574.93976583918584])])):
+    def __init__(self, imgs, lbs, transform=transforms.ToTensor(), img_transform=transforms.Normalize([-634.8493269908545], [548.0778525978939])):
         self.imgs, self.lbs = imgs, lbs
-        self.transform = transform
+        self.transform, self.img_transform = transform, img_transform
     def __len__(self):
         return len(self.imgs)
     def __getitem__(self, index):
         img = self.transform(self.imgs[index]).float()
+        img = self.img_transform(img)
         lb = torch.tensor(self.lbs[index]).float()
         img = img.repeat(3,1,1)
         return (img, lb)
@@ -52,26 +56,27 @@ if __name__ == "__main__":
     parser.add_argument("-stp", metavar="STP", type=int, default=100, dest="stp")
     args = parser.parse_args()
 
-    train_imgs, train_lbs, test_imgs, test_lbs = np.load("train_imgs_1aug.npy"), np.load("weak_train.npy"), np.load("test_imgs_1.npy"), np.load("weak_test.npy") ##
+    train_imgs, train_lbs, test_imgs, test_lbs = np.load("data/COVID_train_imgs.npy"), np.load("data/COVID_train_lbs.npy"), np.load("data/COVID_test_imgs.npy"), np.load("data/COVID_test_lbs.npy") ##
     trainset = Covid(imgs=train_imgs, lbs=train_lbs)
     testset = Covid(imgs=test_imgs, lbs=test_lbs)
 
-    class_counts = [1389.0, 531.0] ##
-    train_label_file = np.load("train_blue.npy") ##
-    weights_per_class = [1920.0/class_counts[i] for i in range(len(class_counts))] ##
-    weights1 = [weights_per_class[train_label_file[i]] for i in range(1920)] ##
-    sampler = data.sampler.WeightedRandomSampler(torch.DoubleTensor(weights1), 1920) ##
-    trainloader = data.DataLoader(trainset, batch_size=20, shuffle=False, sampler=sampler, num_workers=12) ##
-    testloader = data.DataLoader(testset, batch_size=20, shuffle=False, num_workers=12) ##
+    class_counts = [864.0, 555.0] ##
+    weights_per_class = [1419.0/class_counts[i] for i in range(len(class_counts))] ##
+    weights1 = [weights_per_class[train_lbs[i]] for i in range(1419)] ##
+    sampler = data.sampler.WeightedRandomSampler(torch.DoubleTensor(weights1), 1419) ##
+    trainloader = data.DataLoader(trainset, batch_size=3, shuffle=False, sampler=sampler, num_workers=12) ##
+    testloader = data.DataLoader(testset, batch_size=8, shuffle=False, num_workers=12) ##
 
     a = "cuda:" + str(args.cuda)
     device = torch.device(a if torch.cuda.is_available() else "cpu")
     criterion1 = nn.BCEWithLogitsLoss().to(device)
     net = run_cnn()
     val = False
+    alpha = args.lr
     if args.pre is not None:
         checkpoint = torch.load(args.pre)
         net.load_state_dict(checkpoint["net"])
+        alpha = checkpoint["alpha"]
     net.to(device)
 
     if args.opt == "Adam":
@@ -82,12 +87,13 @@ if __name__ == "__main__":
         optimizer = optim.SGD(net.parameters(), lr=alpha, weight_decay=args.wd, momentum=args.m)
     else:
         optimizer = optimizer = optim.RMSprop(net.parameters(), lr=alpha, weight_decay=args.wd)
-    if vall:
+    if val:
         optimizer.load_state_dict(checkpoint["optimizer"])
     
     train_loss, test_loss = [], []
-    start_ = checkpoint["epoch"] if vall else 1
-    epochs = checkpoint["epoch"]+args.eps if vall else args.eps
+    start_ = checkpoint["epoch"] if val else 1
+    best_loss = checkpoint["loss"] if val else 100
+    epochs = checkpoint["epoch"]+args.eps if val else args.eps
 
     try:
         os.mkdir("models")
@@ -100,24 +106,26 @@ if __name__ == "__main__":
         net = net.train()
         epoch_loss = 0.0
         for img, lb in trainloader:
+            lb = lb.reshape(lb.size(0), 1)
             img, lb = img.to(device), lb.to(device)
             lb_hat = net(img)
             loss = criterion1(lb_hat, lb)
             epoch_loss += loss.item()
             optimizer.zero_grad()
             optimizer.step()
-        train_loss.append(epoch_loss/240) ##
-        print("Epoch" + str(epoch) + " Train BCE Loss:", epoch_loss/240) ##
+        train_loss.append(epoch_loss/473) ##
+        print("Epoch" + str(epoch) + " Train BCE Loss:", epoch_loss/473) ##
 
         net = net.eval()
         epoch_loss = 0.0
         with torch.no_grad():
             for img, lb in testloader:
+                lb = lb.reshape(lb.size(0), 1)
                 img, lb = img.to(device), lb.to(device)
                 lb_hat = net(img)
                 loss = criterion1(lb_hat, lb)
                 epoch_loss += loss.item()
-        epoch_loss /= 240
+        epoch_loss /= 19.625
         test_loss.append(epoch_loss)
         print("Epoch" + str(epoch) + " Test BCE Loss:", epoch_loss) ##
 
@@ -138,7 +146,7 @@ if __name__ == "__main__":
                 "optimizer": optimizer.state_dict(),
                 "alpha": alpha_
             }
-            path_ = "./models/" + args.opt + "_lr" + str(args.lr) + "/"
+            path_ = "./models_class/" + args.opt + "_lr" + str(args.lr) + "/"
             try:
                 os.mkdir(path_)
             except:
